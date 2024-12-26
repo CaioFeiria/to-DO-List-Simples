e7d233420c55543dbbb655ba5ac61e10af047fb9
caio <caiofeiria@proton.me>
2024-12-26 20:49:22 -0300

    adicionando campo descrição, alteração validação campos, botão editar e modal

--- js/index.js
+++ js/index.js
@@ -1,137 +1,232 @@
 const minhaLista = document.querySelector("#minhaLI");
 const inputTask = document.querySelector("#input_task");
+const btnSalvar = document.getElementById("salvarTarefa");
+const inputDescricao = document.getElementById("inputDescricao");
+const btnAdd = document.querySelector("#btn_add");
 let arrayLista = [];
 let idTarefa = 0;
+let tarefaEditando = null;
 
-document.querySelector("#btn_add").addEventListener("click", () => {
-    if(inputTask.value !== ""){
+btnAdd.addEventListener("click", () => {
+  if (inputTask.value !== "") {
+    erroInputVazio();
+    criarTarefa();
+    criarItemLista();
+  } else {
+    erroInputVazio();
+  }
+});
+
+inputTask.addEventListener("keydown", (evento) => {
+  if (evento.key === "Enter") {
+    if (inputTask.value !== "") {
+      erroInputVazio();
+      criarTarefa();
+      criarItemLista();
+    } else {
+      erroInputVazio();
+    }
+  }
+});
+
+inputDescricao.addEventListener("keydown", (evento) => {
+    if (evento.key === "Enter") {
+      if (inputTask.value !== "") {
+        erroInputVazio();
         criarTarefa();
         criarItemLista();
+      } else {
         erroInputVazio();
-    } else{
-        erroInputVazio();
+      }
     }
-});
+  });
 
-inputTask.addEventListener("keydown", evento => {
-    if(evento.key === "Enter"){
-        if(inputTask.value !== ""){
-            criarTarefa();
-            criarItemLista();
-            erroInputVazio();
-        } else{
-            erroInputVazio();
-        }
-    }
-});
+function criarTarefa(checkBox = false) {
+  const checkDescricao = document.getElementById("checkDescricao");
 
-function criarTarefa(checkBox = false){
-    const tarefa = {
-        id: ++idTarefa,
-        task: inputTask.value,
-        check: checkBox
-    }
+  const tarefa = {
+    id: ++idTarefa,
+    task: inputTask.value,
+    check: checkBox,
+    descricao: checkDescricao.checked ? inputDescricao.value : "",
+  };
 
-    arrayLista.push(tarefa);
+  arrayLista.push(tarefa);
 }
 
 function criarItemLista() {
-    minhaLista.innerHTML = "";
+  minhaLista.innerHTML = "";
+  btnAdd.classList.remove("btnAddComErroSpan");
 
-    arrayLista.forEach((tarefa) => {
-        const li = document.createElement("li");
-        const span = document.createElement("span");
-        const divCheckSpan = document.createElement("div");
-        const divBtnExcluir = document.createElement("div");
-        const checkBox = document.createElement("input");
-        const btnExcluir = document.createElement("button");
+  arrayLista.forEach((tarefa) => {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    const divCheckSpan = document.createElement("div");
+    const divBtns = document.createElement("div");
+    const checkBox = document.createElement("input");
+    const btnEditar = document.createElement("button");
+    const btnExcluir = document.createElement("button");
 
-        divCheckSpan.classList.add("div_checkSpan");
-        divBtnExcluir.classList.add("div_btnExcluir");
-        li.dataset.id = tarefa.id;
-        li.classList.add("item_lista");
+    divCheckSpan.classList.add("div_checkSpan");
+    divBtns.classList.add("div_btns");
+    li.dataset.id = tarefa.id;
+    li.classList.add("item_lista");
 
-        checkBox.setAttribute("type", "checkbox");
-        checkBox.classList.add("form-check-input");
-        checkBox.checked = tarefa.check;
-        if (checkBox.checked) {
-            span.classList.add("concluido");
-        }
+    checkBox.setAttribute("type", "checkbox");
+    checkBox.classList.add("form-check-input");
+    checkBox.checked = tarefa.check;
+    if (checkBox.checked) {
+      span.classList.add("concluido");
+    }
 
-        span.textContent = tarefa.task;
+    span.textContent = `${tarefa.task} ${tarefa.descricao}`;
 
-        btnExcluir.addEventListener("click", () => {
-            if (tarefa.check === false) {
-                const modalExcluir = new bootstrap.Modal(document.getElementById("modalExcluir"));
-                modalExcluir.show();
-            } else {
-                excluirTarefa(tarefa.id);
-            }
-        });
+    btnEditar.classList.add("btn_editar");
+    btnEditar.addEventListener("click", () => {
+      const modalEditar = new bootstrap.Modal(
+        document.getElementById("editarTarefaModal")
+      );
+      modalEditar.show();
 
-        checkBox.addEventListener("click", () => {
-            tarefa.check = checkBox.checked;
-            if (checkBox.checked) {
-                span.classList.add("concluido");
-            } else {
-                span.classList.remove("concluido");
-            }
-        });
+      tarefaEditando = tarefa;
 
-        divCheckSpan.appendChild(checkBox);
-        divCheckSpan.appendChild(span);
-        divBtnExcluir.appendChild(btnExcluir);
-        li.appendChild(divCheckSpan);
-        li.appendChild(divBtnExcluir);
+      const tituloEdit = document.getElementById("titulo-tarefa");
+      const descricaoEdit = document.getElementById("descricao-tarefa");
 
-        minhaLista.appendChild(li);
+      tituloEdit.value = tarefa.task;
+      descricaoEdit.value = tarefa.descricao;
     });
 
-    inputTask.value = "";
-    inputTask.focus();
+    btnExcluir.classList.add("btn_excluir");
+    btnExcluir.addEventListener("click", () => {
+      if (tarefa.check === false) {
+        const modalExcluir = new bootstrap.Modal(
+          document.getElementById("modalExcluir")
+        );
+        modalExcluir.show();
+      } else {
+        excluirTarefa(tarefa.id);
+      }
+    });
+
+    checkBox.addEventListener("click", () => {
+      tarefa.check = checkBox.checked;
+      if (checkBox.checked) {
+        span.classList.add("concluido");
+      } else {
+        span.classList.remove("concluido");
+      }
+    });
+
+    divCheckSpan.appendChild(checkBox);
+    divCheckSpan.appendChild(span);
+    divBtns.appendChild(btnEditar);
+    divBtns.appendChild(btnExcluir);
+    li.appendChild(divCheckSpan);
+    li.appendChild(divBtns);
+
+    minhaLista.appendChild(li);
+  });
+
+  inputTask.value = "";
+  inputDescricao.value = "";
+  inputTask.focus();
 }
+
+btnSalvar.addEventListener("click", () => {
+  if (tarefaEditando) {
+    const tituloEdit = document.getElementById("titulo-tarefa");
+    const descricaoEdit = document.getElementById("descricao-tarefa");
+
+    tarefaEditando.task = tituloEdit.value;
+    tarefaEditando.descricao = descricaoEdit.value;
+
+    criarItemLista();
+
+    const modalEditar = bootstrap.Modal.getInstance(
+      document.getElementById("editarTarefaModal")
+    );
+    modalEditar.hide();
+
+    tarefaEditando = null;
+  }
+});
 
 function excluirTarefa(id) {
-    for (let i = 0; i < arrayLista.length; i++) {
-        if (arrayLista[i].id == id) {
-            const modalConfirmarExcluir = new bootstrap.Modal(document.getElementById("modalConfirmarExcluir"));
-            modalConfirmarExcluir.show();
+  for (let i = 0; i < arrayLista.length; i++) {
+    if (arrayLista[i].id == id) {
+      const modalConfirmarExcluir = new bootstrap.Modal(
+        document.getElementById("modalConfirmarExcluir")
+      );
+      modalConfirmarExcluir.show();
 
-            document.getElementById("btnConfirmarExcluir").onclick = function() {
-                arrayLista.splice(i, 1);
-                criarItemLista();
-                modalConfirmarExcluir.hide();
-            };
-            break;
-        }
+      document.getElementById("btnConfirmarExcluir").onclick = function () {
+        arrayLista.splice(i, 1);
+        criarItemLista();
+        modalConfirmarExcluir.hide();
+      };
+      break;
     }
+  }
 }
 
+function erroInputVazio() {
+  const inputs = document.querySelectorAll(".inputErro > input");
 
-function erroInputVazio(){
-    const inputs = document.querySelectorAll("#container_form input");
+  inputs.forEach((input) => {
+    const divPai = input.parentNode;
+    const spanExistente = divPai.querySelector(".span_erro");
 
-    inputs.forEach(input => {
-      const divPai = document.querySelector("#div_input_btn").parentNode;
-      const spanExistente = divPai.querySelector(".span_erro");
-  
-      if (spanExistente){
-        spanExistente.remove();
+    if (spanExistente) {
+      spanExistente.remove();
+    }
+
+    if (input.value === "") {
+      let spanMensagem = document.createElement("span");
+
+      if (input.placeholder === "") {
+        btnAdd.classList.add("btnAddComErroSpan");
+        spanMensagem.textContent = "O campo é obrigatório";
+      } else {
+        btnAdd.classList.add("btnAddComErroSpan");
+        spanMensagem.textContent = `Preencha o campo ${input.placeholder}`;
       }
-  
-      if (input.value === ""){
-        const spanMensagem = document.createElement("span");
-  
-        if (input.placeholder === ""){
-          spanMensagem.textContent = "O campo é obrigatório";
-        } else{
-          spanMensagem.textContent = `Preencha o campo ${input.placeholder}`;
-        }
-  
-        spanMensagem.classList.add("span_erro");
-        divPai.appendChild(spanMensagem);
-      }
-    });
 
-    inputTask.focus();
+      spanMensagem.classList.add("span_erro");
+      divPai.appendChild(spanMensagem);
+    }
+
+  });
+
+  inputTask.focus();
 }
+
+// function gerenciarInputDescricao() {
+//   const divPai = document.getElementById("div_input_btn").parentNode;
+
+//   if (!inputDescricao) {
+//     inputDescricao = document.createElement("input");
+//     divInpDesc = document.createElement("div");
+//     divInpDesc.classList.add("inputErro");
+//     inputDescricao.setAttribute("placeholder", "Descrição");
+//     inputDescricao.setAttribute("type", "text");
+//     inputDescricao.id = "inputDescricao";
+//     inputDescricao.classList.add("form-control");
+//     inputDescricao.style.display = "none";
+//     divInpDesc.appendChild(inputDescricao);
+//     divPai.appendChild(divInpDesc);
+//   }
+
+//   document
+//     .getElementById("checkDescricao")
+//     .addEventListener("change", function () {
+//       if (this.checked) {
+//         inputDescricao.style.display = "block";
+//         inputTask.focus();
+//       } else {
+//         inputDescricao.style.display = "none";
+//         inputDescricao.value = "";
+//       }
+//     });
+// }
+// document.addEventListener("DOMContentLoaded", gerenciarInputDescricao);
